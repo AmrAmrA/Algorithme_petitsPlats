59309d0caf1912fcc6eecfbe2a237112b6ca3fdd
Abd El Fattah Amr <amrelfattah.dev@gmail.com>
2023-04-13 20:03:10 +0200

    Displaying recipes cards

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let main = document.querySelector('main')
+
 async function displayInformations() {
     const response = await fetch('recipes.json'); 
     if (!response.ok) {
@@ -9,5 +11,30 @@ async function displayInformations() {
 }
 
 displayInformations().then((data) => {
-    console.log(data);
+    for(let i = 0; i < data.recipes.length; i++) {
+        console.log(data.recipes[i].ingredients);
+        const article = document.createElement('article');
+        article.classList.add('recipe__card')
+        main.appendChild(article)
+        article.innerHTML = `
+        <div class = 'fake__image'> </div>
+        <div class = 'recipe__body'> 
+            <div class = 'recipe__header'>
+                <h5 class = "recipe__title"> ${data.recipes[i].name} </h5>
+                <h5 class = "recipe__time"> <i class="fa-regular fa-clock recipe__clock"></i> ${data.recipes[i].time} min </h5>
+            </div>
+            <div class = "recipe__preparation">
+             <ul>
+                ${data.recipes[i].ingredients.map(ingredient =>  
+                    `<li>  ${ingredient.ingredient}  ${ingredient.quantity} ${ingredient.unit} </li>`) 
+                    .join('')}
+                
+             </ul>
+             <p class = "recipe__ingredients">
+             ${data.recipes[i].description}
+             </p>
+            </div>
+        </div>
+        `
+    }
 })
