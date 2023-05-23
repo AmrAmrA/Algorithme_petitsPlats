adc4a0fc24aeedc12542043e290023664984b791
Abd El Fattah Amr <amrelfattah.dev@gmail.com>
2023-05-23 16:28:02 +0200

    Modifying HTML buttons architecture

--- app.js
+++ app.js
@@ -14,42 +14,45 @@ async function displayInformations() {
 displayInformations().then((data) => {
   const main = document.querySelector("main");
   for (const recipe of data.recipes) {
-    const article = document.createElement("article");
-    article.classList.add("recipe__card");
-    main.appendChild(article);
-    article.innerHTML = `
-        <div class = 'fake__image' > </div>
-        <div class = 'recipe__body   ${recipe.appliance}'> 
-            <div class = 'recipe__header'>
-                <h1 class = "recipe__title"> ${recipe.name} </h1>
-                <span class = "recipe__time"> <i class="fa-regular fa-clock recipe__clock"></i> ${
-                  recipe.time
-                } min </span>
-            </div>
-            <div class = "recipe__preparation">
-             <ul>
-                ${recipe.ingredients
-                  .map(
-                    (ingredient) =>
-                      `<li>  
-                     <span class = "ingredient__name">  ${
-                       ingredient.ingredient
-                     }  </span>   
-                    <span> : ${
-                      ingredient.quantity ? ingredient.quantity : ""
-                    } </span> 
-                    <span>  ${ingredient.unit ? ingredient.unit : ""} </span>
-                    </li>`
-                  )
-                  .join("")}
-                
-             </ul>
-             <p class = "recipe__ingredients">
-             ${recipe.description}
-             </p>
-            </div>
-        </div>
-        `;
+        function createRecipes() {
+          const article = document.createElement("article");
+          article.classList.add("recipe__card");
+          main.appendChild(article);
+          article.innerHTML = `
+              <div class = 'fake__image' > </div>
+              <div class = 'recipe__body   ${recipe.appliance}'> 
+                  <div class = 'recipe__header'>
+                      <h1 class = "recipe__title"> ${recipe.name} </h1>
+                      <span class = "recipe__time"> <i class="fa-regular fa-clock recipe__clock"></i> ${
+                        recipe.time
+                      } min </span>
+                  </div>
+                  <div class = "recipe__preparation">
+                   <ul>
+                      ${recipe.ingredients
+                        .map(
+                          (ingredient) =>
+                            `<li>  
+                           <span class = "ingredient__name">  ${
+                             ingredient.ingredient
+                           }  </span>   
+                          <span> : ${
+                            ingredient.quantity ? ingredient.quantity : ""
+                          } </span> 
+                          <span>  ${ingredient.unit ? ingredient.unit : ""} </span>
+                          </li>`
+                        )
+                        .join("")}
+                      
+                   </ul>
+                   <p class = "recipe__ingredients">
+                   ${recipe.description}
+                   </p>
+                  </div>
+              </div>
+              `;
+        }
+        createRecipes();
   }
 });
 
@@ -134,42 +137,7 @@ displayInformations().then((data) => {
       }
     } else {
       for (const recipe of data.recipes) {
-        const article = document.createElement("article");
-        article.classList.add("recipe__card");
-        main.appendChild(article);
-        article.innerHTML = `
-            <div class = 'fake__image' > </div>
-            <div class = 'recipe__body   ${recipe.appliance}'> 
-                <div class = 'recipe__header'>
-                    <h1 class = "recipe__title"> ${recipe.name} </h1>
-                    <span class = "recipe__time"> <i class="fa-regular fa-clock recipe__clock"></i> ${
-                      recipe.time
-                    } min </span>
-                </div>
-                <div class = "recipe__preparation">
-                 <ul>
-                    ${recipe.ingredients
-                      .map(
-                        (ingredient) =>
-                          `<li>  
-                         <span class = "ingredient__name">  ${
-                           ingredient.ingredient
-                         }  </span>   
-                        <span> : ${
-                          ingredient.quantity ? ingredient.quantity : ""
-                        } </span> 
-                        <span>  ${ingredient.unit ? ingredient.unit : ""} </span>
-                        </li>`
-                      )
-                      .join("")}
-                    
-                 </ul>
-                 <p class = "recipe__ingredients">
-                 ${recipe.description}
-                 </p>
-                </div>
-            </div>
-            `;
+        createRecipes();
     }
   }});
 });
