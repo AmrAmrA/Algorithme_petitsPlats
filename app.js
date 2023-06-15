39343b972adcce805ba163aeb08ebf962442747a
Abd El Fattah Amr <amrelfattah.dev@gmail.com>
2023-06-15 22:57:03 +0200

    reduce the number of API Calls

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Function to call the API asynchronously
-let AllRecipes = [];
+let dataArray = [];
 async function displayInformations() {
   const response = await fetch("recipes.json", Headers);
 
@@ -8,198 +8,176 @@ async function displayInformations() {
     throw new Error(message);
   }
   const data = await response.json();
+    dataArray.push(data.recipes)
+
   return data;
 }
 
-// Tu as toujours ton AllRecipes qui contient toutes les recettes
-const filteredRecipes = [];
-// FilterdRecipes est un tableau qui va contenir une copie de AllRecipes
-// Lorsque tu dois filtrer et du coup afficher sur le DOM, c'est sur FilteredRecipes que tu dois travailler
-
-// Lorsque tu ajoutes un tag > Tu filtre et tu mets à jour filteredRecipes
-// Tu peux donc à nouveau parcourir filteredRecipes pour afficher les tags mis à jour
-
-// Function to display the recipes with their titles, time and ingredients
-displayInformations().then((data) => {
+function fillTheDom() {
   const main = document.querySelector("main");
-  for (const recipe of data.recipes) {
-    function createRecipes() {
-      const article = document.createElement("article");
-      article.classList.add("recipe__card");
-      main.appendChild(article);
-      article.innerHTML = `
+  for(const recipe of dataArray[0]){
+    const article = document.createElement("article");
+    article.classList.add("recipe__card");
+    main.appendChild(article);
+    article.innerHTML = `
               <div class = 'fake__image' > </div>
-              <div class = 'recipe__body   ${recipe.appliance}'> 
+              <div class = 'recipe__body'> 
                   <div class = 'recipe__header'>
                       <h1 class = "recipe__title"> ${recipe.name} </h1>
-                      <span class = "recipe__time"> <i class="fa-regular fa-clock recipe__clock"></i> ${
-                        recipe.time
-                      } min </span>
+                      <span class = "recipe__time"> <i class="fa-regular fa-clock recipe__clock"></i> ${recipe.time} min </span>
                   </div>
                   <div class = "recipe__preparation">
                    <ul>
-                      ${recipe.ingredients
-                        .map(
-                          (ingredient) =>
-                            `<li>  
-                           <span class = "ingredient__name">  ${
-                             ingredient.ingredient
-                           }  </span>   
-                          <span> : ${
-                            ingredient.quantity ? ingredient.quantity : ""
-                          } </span> 
-                          <span>  ${
-                            ingredient.unit ? ingredient.unit : ""
-                          } </span>
+                      ${recipe.ingredients.map((ingredient) =>
+                          `<li>  
+                              <span class = "ingredient__name">  ${ingredient.ingredient} </span>   
+                              <span> : ${ingredient.quantity ? ingredient.quantity : ""} </span> 
+                              <span>  ${ingredient.unit ? ingredient.unit : ""} </span>
                           </li>`
-                        )
-                        .join("")}
-                      
+                        ).join("")}
                    </ul>
-                   <p class = "recipe__ingredients">
-                   ${recipe.description}
-                   </p>
+                   <p class = "recipe__ingredients">${recipe.description}</p>
                   </div>
               </div>
               `;
-    }
-    createRecipes();
   }
-});
+}
+setTimeout(fillTheDom, 500);
+
 
 const queryInput = document.querySelector(".query__input");
 const emptyArray = [];
-displayInformations().then((data) => {
-  // First step: create an array with all the ingredients, appliances and ustensils
-  for (recipe of data.recipes) {
-    emptyArray.push(recipe.appliance);
-    emptyArray.push(recipe.ustensils);
+// displayInformations().then((data) => {
+//   // First step: create an array with all the ingredients, appliances and ustensils
+//   for (recipe of data.recipes) {
+//     emptyArray.push(recipe.appliance);
+//     emptyArray.push(recipe.ustensils);
 
-    for (const ingredients of recipe.ingredients) {
-      emptyArray.push(ingredients.ingredient);
-    }
-  }
-  // Second step: remove the duplicates from the array
-  // and use flat method to remove the nested arrays
-  const newArrayWithoutDuplicates = [...new Set(emptyArray)];
-  const LatestArrayVersion = newArrayWithoutDuplicates.flat();
+//     for (const ingredients of recipe.ingredients) {
+//       emptyArray.push(ingredients.ingredient);
+//     }
+//   }
+//   // Second step: remove the duplicates from the array
+//   // and use flat method to remove the nested arrays
+//   const newArrayWithoutDuplicates = [...new Set(emptyArray)];
+//   const LatestArrayVersion = newArrayWithoutDuplicates.flat();
 
-  // The user types in the input and the recipes are filtered
-  // if the value matches with the ingredients, appliances or ustensils
-  // we will display some recipes if not, we will display all the recipes
-  const main = document.querySelector("main");
-  queryInput.addEventListener("keyup", (e) => {
-    main.innerHTML = "";
-    const inputValue = e.target.value;
-    if (LatestArrayVersion.includes(inputValue)) {
-      for (oneRecipe of data.recipes) {
-        const article = document.createElement("article");
-        article.classList.add("recipe__card");
-        const flatUstensils = oneRecipe.ustensils.flat();
-        const IngredientsArray = [];
-        for (const ingredient of oneRecipe.ingredients) {
-          IngredientsArray.push(ingredient.ingredient);
-        }
-        if (
-          oneRecipe.appliance === inputValue ||
-          oneRecipe.ustensils.includes(inputValue) ||
-          IngredientsArray.includes(inputValue)
-        ) {
-          updateIngredientsList(inputValue);
-          updateAppliancesList(inputValue);
-          updateUstensilsList(inputValue);
-          main.appendChild(article);
-          article.innerHTML = `
-        <div class = 'fake__image' > </div>
-        <div class = 'recipe__body   ${oneRecipe.appliance}'> 
-            <div class = 'recipe__header'>
-                <h1 class = "recipe__title"> ${oneRecipe.name} </h1>
-                <span class = "recipe__time"> <i class="fa-regular fa-clock recipe__clock"></i> ${
-                  oneRecipe.time
-                } min </span>
-            </div>
-            <div class = "recipe__preparation">
-             <ul>
-                ${oneRecipe.ingredients
-                  .map(
-                    (ingredient) =>
-                      `<li>  
-                     <span class = "ingredient__name">  ${
-                       ingredient.ingredient
-                     }  </span>   
-                    <span> : ${
-                      ingredient.quantity ? ingredient.quantity : ""
-                    } </span> 
-                    <span>  ${ingredient.unit ? ingredient.unit : ""} </span>
-                    </li>`
-                  )
-                  .join("")}
+//   // The user types in the input and the recipes are filtered
+//   // if the value matches with the ingredients, appliances or ustensils
+//   // we will display some recipes if not, we will display all the recipes
+//   const main = document.querySelector("main");
+//   queryInput.addEventListener("keyup", (e) => {
+//     main.innerHTML = "";
+//     const inputValue = e.target.value;
+//     if (LatestArrayVersion.includes(inputValue)) {
+//       for (oneRecipe of data.recipes) {
+//         const article = document.createElement("article");
+//         article.classList.add("recipe__card");
+//         const flatUstensils = oneRecipe.ustensils.flat();
+//         const IngredientsArray = [];
+//         for (const ingredient of oneRecipe.ingredients) {
+//           IngredientsArray.push(ingredient.ingredient);
+//         }
+//         if (
+//           oneRecipe.appliance === inputValue ||
+//           oneRecipe.ustensils.includes(inputValue) ||
+//           IngredientsArray.includes(inputValue)
+//         ) {
+//           updateIngredientsList(inputValue);
+//           updateAppliancesList(inputValue);
+//           updateUstensilsList(inputValue);
+//           main.appendChild(article);
+//           article.innerHTML = `
+//         <div class = 'fake__image' > </div>
+//         <div class = 'recipe__body   ${oneRecipe.appliance}'> 
+//             <div class = 'recipe__header'>
+//                 <h1 class = "recipe__title"> ${oneRecipe.name} </h1>
+//                 <span class = "recipe__time"> <i class="fa-regular fa-clock recipe__clock"></i> ${
+//                   oneRecipe.time
+//                 } min </span>
+//             </div>
+//             <div class = "recipe__preparation">
+//              <ul>
+//                 ${oneRecipe.ingredients
+//                   .map(
+//                     (ingredient) =>
+//                       `<li>  
+//                      <span class = "ingredient__name">  ${
+//                        ingredient.ingredient
+//                      }  </span>   
+//                     <span> : ${
+//                       ingredient.quantity ? ingredient.quantity : ""
+//                     } </span> 
+//                     <span>  ${ingredient.unit ? ingredient.unit : ""} </span>
+//                     </li>`
+//                   )
+//                   .join("")}
                 
-             </ul>
-             <p class = "recipe__ingredients">
-             ${oneRecipe.description}
-             </p>
-            </div>
-        </div>
-        `;
-        }
-      }
-    } else {
-      const listIngredients = document.querySelector(".listIngredients");
-      listIngredients.innerHTML = "";
-      const listAppliances = document.querySelector(".listAppliances");
-      listAppliances.innerHTML = "";
-      const listUstensils = document.querySelector(".listUstensils");
-      listUstensils.innerHTML = "";
-      fillIngredientsList();
-      fillAppliancesList();
-      fillUstensilsList();
-      for (const recipe of data.recipes) {
-        function createRecipes() {
-          const article = document.createElement("article");
-          article.classList.add("recipe__card");
-          main.appendChild(article);
-          article.innerHTML = `
-              <div class = 'fake__image' > </div>
-              <div class = 'recipe__body   ${recipe.appliance}'> 
-                  <div class = 'recipe__header'>
-                      <h1 class = "recipe__title"> ${recipe.name} </h1>
-                      <span class = "recipe__time"> <i class="fa-regular fa-clock recipe__clock"></i> ${
-                        recipe.time
-                      } min </span>
-                  </div>
-                  <div class = "recipe__preparation">
-                   <ul>
-                      ${recipe.ingredients
-                        .map(
-                          (ingredient) =>
-                            `<li>  
-                           <span class = "ingredient__name">  ${
-                             ingredient.ingredient
-                           }  </span>   
-                          <span> : ${
-                            ingredient.quantity ? ingredient.quantity : ""
-                          } </span> 
-                          <span>  ${
-                            ingredient.unit ? ingredient.unit : ""
-                          } </span>
-                          </li>`
-                        )
-                        .join("")}
+//              </ul>
+//              <p class = "recipe__ingredients">
+//              ${oneRecipe.description}
+//              </p>
+//             </div>
+//         </div>
+//         `;
+//         }
+//       }
+//     } else {
+//       const listIngredients = document.querySelector(".listIngredients");
+//       listIngredients.innerHTML = "";
+//       const listAppliances = document.querySelector(".listAppliances");
+//       listAppliances.innerHTML = "";
+//       const listUstensils = document.querySelector(".listUstensils");
+//       listUstensils.innerHTML = "";
+//       fillIngredientsList();
+//       fillAppliancesList();
+//       fillUstensilsList();
+//       for (const recipe of data.recipes) {
+//         function createRecipes() {
+//           const article = document.createElement("article");
+//           article.classList.add("recipe__card");
+//           main.appendChild(article);
+//           article.innerHTML = `
+//               <div class = 'fake__image' > </div>
+//               <div class = 'recipe__body   ${recipe.appliance}'> 
+//                   <div class = 'recipe__header'>
+//                       <h1 class = "recipe__title"> ${recipe.name} </h1>
+//                       <span class = "recipe__time"> <i class="fa-regular fa-clock recipe__clock"></i> ${
+//                         recipe.time
+//                       } min </span>
+//                   </div>
+//                   <div class = "recipe__preparation">
+//                    <ul>
+//                       ${recipe.ingredients
+//                         .map(
+//                           (ingredient) =>
+//                             `<li>  
+//                            <span class = "ingredient__name">  ${
+//                              ingredient.ingredient
+//                            }  </span>   
+//                           <span> : ${
+//                             ingredient.quantity ? ingredient.quantity : ""
+//                           } </span> 
+//                           <span>  ${
+//                             ingredient.unit ? ingredient.unit : ""
+//                           } </span>
+//                           </li>`
+//                         )
+//                         .join("")}
                       
-                   </ul>
-                   <p class = "recipe__ingredients">
-                   ${recipe.description}
-                   </p>
-                  </div>
-              </div>
-              `;
-        }
-        createRecipes();
-      }
-    }
-  });
-});
+//                    </ul>
+//                    <p class = "recipe__ingredients">
+//                    ${recipe.description}
+//                    </p>
+//                   </div>
+//               </div>
+//               `;
+//         }
+//         createRecipes();
+//       }
+//     }
+//   });
+// });
 
 // ------------- Buttons ranges // Labels ----------------- //
 const labels = document.querySelectorAll("label");
@@ -455,7 +433,6 @@ function searchByIngredientsButton() {
   ingredientsItems.forEach((ingredientItem) => {
     ingredientItem.addEventListener("click", () => {
       ingredientItem.style.pointerEvents = "none";
-      ingredientItem.classList.add("hide");
 
       // Update ingredientsItems List
       updateIngredientsList(ingredientItem.outerText); // Argument
